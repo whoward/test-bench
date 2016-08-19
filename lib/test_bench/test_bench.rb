ebe532efe32eb6342dabcff0bd2e9292acacb555
Nathan Ladd <nathan@obsidianexchange.com>
2016-08-19 14:17:26 -0500

    Configure output immediately

--- lib/test_bench/test_bench.rb
+++ lib/test_bench/test_bench.rb
@@ -1,5 +1,10 @@
 module TestBench
   def self.activate
+    # Monkeypatch assert, context, and test onto the main object
+    unless TOPLEVEL_BINDING.receiver.is_a? Structure
+      TOPLEVEL_BINDING.receiver.extend Structure
+    end
+
     # Ruby pre 2.2 did not implement Binding#receiver
     unless TOPLEVEL_BINDING.respond_to? :receiver
       ::Binding.class_exec do
@@ -8,27 +13,21 @@ module TestBench
         end
       end
     end
-
-    # TestBench.activate can be called multiple times
-    return if TOPLEVEL_BINDING.receiver.is_a? Structure
-
-    # "Monkeypatch" assert, context, and test onto the main object
-    TOPLEVEL_BINDING.receiver.extend Structure
-
-    # Build the output instance that will be used to render all the test output
-    output = Output.build
-
-    # Settings can affect the output (verbosity & color), so we pass the output
-    # object into the toplevel settings object and then apply environment
-    # variables to it.
-    settings = Settings::Registry.get TOPLEVEL_BINDING
-    settings.writer = output.writer
-    Settings::Environment.(settings)
-
-    # Configure reverse backtraces on output
-    output.reverse_backtraces = settings.reverse_backtraces
-
-    # Telemetry pushes updates to output for display
-    Telemetry.subscribe output
   end
+
+  # Build the output instance that will be used to render all the test output
+  output = Output.build
+
+  # Settings can affect the output (verbosity & color), so we pass the output
+  # object into the toplevel settings object and then apply environment
+  # variables to it.
+  settings = Settings::Registry.get TOPLEVEL_BINDING
+  settings.writer = output.writer
+  Settings::Environment.(settings)
+
+  # Configure reverse backtraces on output
+  output.reverse_backtraces = settings.reverse_backtraces
+
+  # Telemetry pushes updates to output for display
+  Telemetry.subscribe output
 end
